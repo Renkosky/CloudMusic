146a6115a67c0b69ee5d94b1efe3be4045ebb804
Renko <philsky28@gmail.com>
2017-08-16 20:55:02 +0800

    8-16 css for playlist

--- src/components/playList/playList.vue
+++ src/components/playList/playList.vue
@@ -1,12 +1,23 @@
 <template>
     <div class="playList" >
-      <div class="playList-head" :style="'backgroundImage:'+'url('+url+')'">
-        <div class="playList-innerhead">
-          <div class="playList-cover"><img :src=" playListData.coverImgUrl "></img></div>
-          <div class="playList-title"></div>         
-        </div>
-        <div class="toolbar"></div>
+      <div class="head-wrap">
+        <div class="playList-head" :style="'backgroundImage:'+'url('+url+')'">
+          </div>
+          <div class="playList-innerhead">
+            <div class="playList-cover"><img :src=" playListData.coverImgUrl "></img></div>
+            <div class="playList-title">{{ playListData.name }}</div>         
+          </div>
+          <div class="toolbar">
+            <ul>
+              <li class="iconfont">&#xe602;<p>{{ playListData.subscribedCount }}</p></li>
+              <li class="iconfont">&#xe6d9;<p>{{ playListData.commentCount }}</p></li>
+              <li class="iconfont">&#xe6db;<p>{{ playListData.shareCount }}</p></li>
+              <li class="iconfont">&#xe62d;<p>下载</p></li>
+            </ul>
+          </div>
+          
       </div>
+      <div class="songlist" v-for="(item,index) in playListData.trackIds">{{ index+1 }}</div>
     </div>
 </template>
 
@@ -18,6 +29,9 @@ export default {
   // data() {
   //   playListData:[]
   // },
+  computed: {
+
+  },
   created: function() {
     console.log(this.$route.params.id)
     api.getPlayListData(this.$route.params.id).then(response => {
@@ -42,6 +56,29 @@ export default {
     margin: 0;
     padding: 0;
 }
+
+
+
+@font-face {
+  font-family: 'iconfont';
+  /* project id 369853 */
+  src: url('//at.alicdn.com/t/font_369853_l9xy2hu0418aor.eot');
+  src: url('//at.alicdn.com/t/font_369853_l9xy2hu0418aor.eot?#iefix') format('embedded-opentype'),
+  url('//at.alicdn.com/t/font_369853_l9xy2hu0418aor.woff') format('woff'),
+  url('//at.alicdn.com/t/font_369853_l9xy2hu0418aor.ttf') format('truetype'),
+  url('//at.alicdn.com/t/font_369853_l9xy2hu0418aor.svg#iconfont') format('svg');
+}
+
+
+.iconfont {
+  font-family: "iconfont" !important;
+  font-style: normal;
+  font-size: 60px;
+  -webkit-font-smoothing: antialiased;
+  -webkit-text-stroke-width: 0.2px;
+  -moz-osx-font-smoothing: grayscale;
+}
+
 .playList{
   position: fixed;
   z-index: 1;
@@ -52,39 +89,72 @@ export default {
   background-color: white;
   
 }
-.playList-head{
-  display: flex;
-  flex-direction:column;
+.head-wrap{
+  position: relative;
   height: 40%;
-  background-size: cover;
   overflow: hidden;
-  border: 4px solid red;
+  color: white
+}
+.playList-head{
+  display: flex; 
+  height: 100%;
+  width: 100%; 
+  overflow:hidden;
+  filter:blur(150px);
+  background-size: cover;
 }
 .playList-innerhead{
   display: flex;
-  height: 20%;
+  position: absolute;
+  top: 0;
   flex: auto; 
-  margin: 10% 5% 5% 5%;
-  z-index:8;
-  filter: blur(0px); 
+  margin: 15% 5% 5% 5%;
 }
 .playList-cover{
-  flex: 1 1;
- 
-  padding: 0px 80px 0px 0px
+  flex: 0 1 300px;
 }
 
 .playList-cover img{
    width: 100%;
    height: 100%;
+  
 }
 
 .playList-title{
-  flex: 1
+  padding-left: 10%;
+  flex: 1;
+  font-size: 35px;
+ 
 }
 .toolbar{
-  flex: 1;
+  position: absolute;
+  display: flex;
+  bottom: 0;
+  height: 25%;
   width: 100%;
+}
+
+.toolbar ul{
+  display: flex;
+  width: 100%;
+  list-style: none;
+  align-items:center
+}
+
+.toolbar ul li{
+  flex: 1;
+  text-align: center;
+  padding: 30px
+}
+
+.toolbar ul li p{
+  font-size: 30px;
+  font-family: "Microsoft YaHei"
+}
+
+.songlist{
+  font-size: 40px;
+  height: 90px;
 }
 
 </style>
