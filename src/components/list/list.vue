1748588ffd9e53b5aec5737834bdeb46ac8e4569
Renko <philsky28@gmail.com>
2017-08-28 23:18:30 +0800

    8-28

--- src/components/list/list.vue
+++ src/components/list/list.vue
@@ -1,32 +1,44 @@
 <template>
-  <div class="highquality">
-    <div class="highqualityBg" :style="'backgroundImage:'+'url('+BgUrl+')'"></div>
-      <div class="list-Img">
-        <img :src="highqualityList[0].coverImgUrl" alt="#">
-      </div>
-     <div class="list-title">
-
-     </div>
-      
+  <div>
+    <div class="highquality">
+      <div class="highqualityBg" :style="'backgroundImage:'+'url('+BgUrl+')'"></div>
+        <div class="list-Img">
+          <img :src="highqualityList[0].coverImgUrl" alt="#">
+        </div>
+      <div class="list-title">
+        <div class="highquality-list">精品歌单 ></div>
+        <div class="list-description">
+          <p>{{ name }}</p>     
+        </div>
+        <div class="list-copywriter"> <p>{{ copywriter }}</p></div>
+      </div>    
+    </div>
+    <personalized :personalized="highqualityList"></personalized>
   </div>
-
 </template>
 
 <script>
 import api from '../../api/api'
+import personalized from '../personalized'
 export default {
   name: 'music-list',
+  components: {
+    personalized
+  },
   data() {
     return {
       highqualityList: [],
-      BgUrl: []
+      BgUrl: [],
+      name: [],
+      copywriter: []
     }
   },
   created: function () {
     api.getApidata('top/playlist/highquality').then(response => {
       this.highqualityList = response.data.playlists
       this.BgUrl = this.highqualityList[0].coverImgUrl
-      console.log(this.highqualityList)
+      this.name = this.highqualityList[0].name
+      this.copywriter = this.highqualityList[0].copywriter
     }, response => {
       console.log('Error')
     })
@@ -51,25 +63,40 @@ export default {
   display: flex;
   width: 100%;
   height: 100%;
-  filter:blur(150px);
+  filter:blur(80px);
   background-size: cover;
 }
 .list-Img{
-  width: 30%;
-  height: 30%;
   position: absolute;
-  top: 20%;
-
+  width: 30%;
+  height: 18%;
+  top: 17%;
+  left: 5%
 }
 
 .list-Img img{
-  width: 100%;
   height: 100%;
 }
 .list-title{
-  width: 70%;
-  height: 80%;
-  margin: 60px 30px 20px 20px;
+  position: absolute;
+  width: 65%;
+  height: 18%;
+  top: 17%;
+  left: 42%;
+}
+.highquality-list{
+ margin-bottom: 30px;
+ font-size: 40px;
+ color: white
 }
 
+.list-description{
+  margin-bottom: 30px; 
+  font-size: 30px;
+  color: white
+}
+.list-copywriter{
+  font-size: 20px;
+  color: rgba(255, 255, 255, 0.6)
+}
 </style>
